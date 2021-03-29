c2880bd709865a31673755f421ffd09abc48a661
Yann <yann.pomie@etu.univ-amu.fr>
2021-03-29 09:47:07 +0200

    vérification lors de l'inscription

--- js/db.js
+++ js/db.js
@@ -12,30 +12,53 @@ const CSS_ERROR = {
 let signup = () => {
     let $data = $("#signup").serializeArray();
     console.log($data)
-    console.log($data[0]['value'])
+    $("#msg").html("")
+    let echec = false
 
 
     //on vérifie la validité des données :
     //TODO afficher les erreurs dans ue notif
     //est ce que les emails sont les mêmes ?
     if ($data[1]['value'] === $data[2]['value']){
-        if ($data[1]['value'].length > 254) $("#signup").append("la longueur de l'email est de plus de 254 charactères!")
-        else if ($data[1]['value'].match(/[A-Z][a-z]/) == null) //TODO finir la regex et le checking
-            ;
+        //sont ils pas trop longs ?
+        if ($data[1]['value'].length > 254 || $data[1]['value'].length === 0){
+            $("#msg").html("la longueur de l'email est de plus de 254 caractères ou nulle!").css(CSS_ERROR)
+            echec = true
+        }
+        //sont ils réellement des mails ?
+        else if ($data[1]['value'].match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) == null) {// source de la regex: https://stackoverflow.com/a/46181
+            $("#msg").html("le mail n'est pas valide!").css(CSS_ERROR);
+            echec = true
+        }
     }
     else{
-        $("#signup").append("les emails ne sont pas les mêmes!")
+        $("#msg").html("les emails ne sont pas les mêmes!").css(CSS_ERROR)
+        echec = true
     }
 
+    //on vérifie les mots de passe
+    if ($data[3]['value'] === $data[4]['value']){
+        //sont ils pas trop longs ?
+        if ($data[3]['value'].length > 72 || $data[3]['value'].length === 0){
+            $("#msg").html("la longueur du mot de passe est de plus de 72 caractères ou nulle!").css(CSS_ERROR)
+            echec = true
+        }
+    }
+    else
+    {
+        $("#msg").html("les mots de passe ne sont pas les mêmes!").css(CSS_ERROR)
+        echec = true
+    }
 
     $("#signup").submit(function (event) {
+        if (echec == true) return false //on empêche l'envoi en cas de pb
         $.ajax({
             url: SIGNUP,
             method: "post",
             data: $data,
             dataType: "json"
         })
-        return false; // pour éviter de refresh la page
+        return false; // pour empêcher le refresh de la page
     })
     // changeView("feed")
 }
@@ -49,4 +72,3 @@ let writepost = () => {
 let getLatestPosts = (n) => {
     //var query = "SELECT id_post, date, content, id_user FROM POST"
 }
-
